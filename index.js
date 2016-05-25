3d50ea1295e62ce09c9bd2a705e463aa6ab9acf2
efkan <efkan_turen@yahoo.com>
2016-05-25 21:05:40 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
-import { NativeModules } from 'react-native';
-module.exports = NativeModules.SystemSettings;
+import {NativeModules} from 'react-native'
+var SystemSettings = NativeModules.SystemSettings
+
+
+function get (
+	callback?: ?(result: ?object) => void
+): Promise {
+	return new Promise((resolve, reject) => {
+		SystemSettings.get(function(result){
+			var value = result ? result : null
+			callback && callback(value)
+			resolve(value)
+		})
+	})
+}
+
+
+module.exports = {
+	get,
+}
